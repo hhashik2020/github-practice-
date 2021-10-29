17e6682e1c53e2999526e78802b1425760e32f8a
hhashik2020 <63142279+hhashik2020@users.noreply.github.com>
2021-10-30 05:04:03 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,69 +1,91 @@
-// ignore_for_file: unnecessary_string_escapes
 
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  var dog = Dog();
+  print('');
+  
+  var dog2= Dog.childNamedConstructor();
+  print('');
+  
+  var dog3 = Dog.namedConstructor();
+  print('');
+  
+  var dog4 = Cat();
+  print(dog4.animal());
+  
+  var dog5 = Cat();
+  print(dog5.dog());
+  
+  
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
 
-  void answerQuestion() {
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
-    
+class Animal {
+  
+  animal(){
+    print('Welcome to the Animal House');
   }
+  
+  //constructor
+  Animal (){
+    print('This is Animal Constructor');
+  }
+  Animal.parentNamedConstructor(){
+    print('This is Animal Parent constructor');
+  }
+  Animal.namedConstructor(){
+    print('This is Animal Named Construcor');
+  }
+  
+  
+ 
+}
 
-  @override
-  Widget build(BuildContext context) {
-    var question = [
-      "Whats\'s your favourite animal ?",
-      "What\'s your favourite color ? "
-    ];
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Demo"),
-        ),
-        body: Column(
-          children: [
-            Text(question[questionIndex]),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  answerQuestion;
-                },
-                child: const Text("Button One")),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  answerQuestion;
-                },
-                child: const Text("Button Two")),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  answerQuestion;
-                },
-                child: const Text("Button Three")),
-          ],
-        ),
-      ),
-    );
+
+class Dog extends Animal {
+  
+  dog(){
+    print('');
+  }
+  
+  //construtctor
+  Dog ():super(){
+    print('This is Dog Constructor');
+  }
+  Dog.childNamedConstructor(){
+    print('This is Dog Child Named Constructor');
+  }
+  Dog.namedConstructor():super.namedConstructor(){
+    print('This is Dog Named Construcor');
   }
 }
+
+class Cat implements Animal,Dog{
+  @override
+  animal(){
+    print('Welcome to the cat House');
+  }
+  @override
+  dog(){
+    print('My pet bread is Pershian Cat');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if we passing paramiter to the main class then we need to pass paramiter to the super;
+//if we ChildClass to the child constructor then we need to pass paramiter to the main function;
