fc0b889d311a82890dd66fc95ed82386d62f3ee1
hhashik2020 <63142279+hhashik2020@users.noreply.github.com>
2021-12-16 03:15:57 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,58 +1,51 @@
+
 import 'package:flutter/material.dart';
-import 'package:snackbar/snack.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool changeTheme = false;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.amber.shade700,
+          primarySwatch: Colors.red,
+          scaffoldBackgroundColor: Colors.blueGrey.shade300,
+          textTheme: const TextTheme(
+              bodyText2: TextStyle(color: Colors.white, height: 50))),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.pink,
+          textTheme: const TextTheme(
+              bodyText2: TextStyle(color: Colors.white, height: 50))),
+      themeMode: changeTheme ? ThemeMode.light : ThemeMode.light,
       home: Scaffold(
-        body: MySnack(),
-      ),
-    );
-  }
-}
 
-class MySnack extends StatefulWidget {
-  const MySnack({Key? key}) : super(key: key);
-
-  @override
-  State<MySnack> createState() => _MySnackState();
-}
-
-class _MySnackState extends State<MySnack> {
-  bool ligton = false;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: FractionalOffset.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-                onTap: () {
-                  setState(() {
-                    ligton = !ligton;
-                  });
-                },
-                child: IconButton(
-                  icon: Icon(
-                    Icons.lightbulb_outline_sharp,
-                    size: 60,
-                    color: ligton ? Colors.red : Colors.yellow,
-                  ),
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Welcomed Sir!!!')));
-                  },
-                )),
-          ],
+        body: Center(
+          child: IconButton(
+              onPressed: () {
+                setState(() {
+                  changeTheme = !changeTheme;
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Your Theme is Changed !!!')));
+              },
+              icon: Icon(
+                changeTheme ? Icons.change_circle : Icons.done,
+                color: changeTheme?Colors.white:Colors.white,
+                size: 60,
+              )),
         ),
       ),
     );
